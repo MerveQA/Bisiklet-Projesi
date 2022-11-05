b42040185e09bf112f391a9d97a31d81079f4227
M.Merve KOŞAR <merveabd.61@gmail.com>
2022-11-06 01:23:45 +0300

    6 Kasim

---
+++ Runner.java
@@ -0,0 +1,12 @@
+package j19_StaticKeyword.Task04_Bicycle;
+
+public class Runner {
+
+    public static void main(String[] args) {  // main method static
+
+        Menu m1 = new Menu();
+        m1.bisikletmenu();
+    }
+
+
+}
